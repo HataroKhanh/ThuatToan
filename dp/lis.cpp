6a11cbdfdcaeb7cef55ba204f9a0477cd3665064
Khanh <khanhdacodau2007@gmail.com>
2024-05-25 11:40:38 +0700

    lis dp

---
+++ dp/lis.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, a[1007], l[1007], ans = 1;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    sudo pacman - S neovide for (int i = 1; i <= n; i++)
+    {
+        l[i] = 1;
+        for (int j = 1; j < i; j++)
+        {
+            if (a[i] > a[j])
+            {
+                l[i] = max(l[i], l[j] + 1);
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        ans = max(ans, l[i]);
+        cout << l[i] << ' ';
+        cout << '\n';
+    }
+    cout << ans;
+
+    return 0;
+}
